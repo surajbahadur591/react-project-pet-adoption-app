b0674715b7fd4435ad7bf1bd1a0ab61136b828d8
Suraj <surajbahadur58@gmail.com>
2022-12-29 22:34:04 +0530

    styling for details page

--- src/Details.jsx
+++ src/Details.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query';
 import fetchPet from './fetchPet';
-
+import location from './shared/location.svg'
 
 const Details = () => {
 const {id} = useParams();
@@ -10,8 +10,16 @@ const {id} = useParams();
 // param 1 : cacheid name 
 // param 2 : id 
 // param 3 : call function 
+
 const results = useQuery(["details", id], fetchPet);
 
+  let SEX = "";
+  // logic to convet id to gender of animal
+  if (id % 2 === 0) {
+    SEX = "Male";
+  } else {
+    SEX = "Female";
+  }
 if (results.isLoading) {
   return ( 
     <div className='loading-pane'>
@@ -25,11 +33,16 @@ if (results.isLoading) {
   return (
     <div className='details'>
       <div>
-        <h1>{pet.name}</h1>
-        <h2>{pet.animal} - {pet.breed} - {pet.city}, {pet.state} </h2>
-        <img src={pet.images[0]} alt="" width="200px" />
-        <h3>{pet.description}</h3>
-        <button>Adopt {pet.name}</button>
+        <h2 className='details-font'>{pet.name}</h2>
+        <h2 className='details-font' > <img src={location} height='20px' alt="" /> {pet.city}, {pet.state} </h2>
+        <h2 className='details-font' >Breed - {pet.breed}</h2>
+        <h2 className='details-font'>Sex - {SEX}</h2>
+        <h2 className='details-font' >Age - {pet.name.length}</h2>
+        
+        <img src={pet.images[0]} alt="" width="250px" />
+        <button className='searchbutton-fix' >Adopt {pet.name}</button>
+        
+        <h4>{pet.description}</h4>
       </div>
     </div>
   )
